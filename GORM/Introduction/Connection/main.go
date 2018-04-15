c07cb70bc8fa63a4cf31feb057e8dda086486d09
Niraj-Fonseka <fonseka.live@utexas.edu>
2018-04-15 18:36:14 -0500

    Adding Gcloud and GORM

--- GORM/Introduction/Connection/main.go
+++ GORM/Introduction/Connection/main.go
@@ -1,5 +1,28 @@
 package main
 
 import (
-	_ "lib/pq"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
+
+func main() {
+
+	db, err := gorm.Open("postgres", "user=gorm password=gorm dbname=gorm sslmode=disable")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	dbase := db.DB()
+	defer db.Close()
+
+	err = dbase.Ping()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Database connection established")
+}
