903c6460902dd7035f55a44522bec28a660fa142
root <root@DESKTOP-D1U71RA.localdomain>
2018-05-08 23:22:43 -0500

    Using reflect to tap into some struct mem layout

---
+++ MemoryLayout/Mem.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type MyData struct {
+	aByte   byte
+	aShort  int16
+	anInt32 int32
+	aSlice  []byte
+}
+
+func main() {
+
+	typ := reflect.TypeOf(MyData{})
+	fmt.Printf("Struct is %d bytes long\n", typ.Size())
+	// We can run through the fields in the structure in order
+	n := typ.NumField()
+	for i := 0; i < n; i++ {
+		field := typ.Field(i)
+		fmt.Printf("%s at offset %v, size=%d, align=%d\n",
+			field.Name, field.Offset, field.Type.Size(),
+			field.Type.Align())
+	}
+}
