476493f9aa6ac98f1c958899d921770f55b42dbd
MNF4691 <m_n_fonseka@homedepot.com>
2018-05-30 17:36:37 -0500

    Trying to check the function comments

--- GoDoc/packageDoc/packageDoc.go
+++ GoDoc/packageDoc/packageDoc.go
@@ -3,7 +3,8 @@ package packageDoc
 
 import "fmt"
 
-//This is a package-level example:
+// This is a package-level example:
+// This is the second line of the package level example
 func Example() {
 	fmt.Printf("Hi")
 }
