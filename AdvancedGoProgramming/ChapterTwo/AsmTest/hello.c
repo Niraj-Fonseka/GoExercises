25a5e80e2bd0a75ec5c2515a622be7e4b8041587
MNF4691 <m_n_fonseka@homedepot.com>
2018-08-21 11:52:33 -0500

    Adding some assembly and mixing it with go and cgo

--- AdvancedGoProgramming/ChapterTwo/AsmTest/hello.c
+++ AdvancedGoProgramming/ChapterTwo/AsmTest/hello.c
@@ -5,9 +5,26 @@
 A clobbered register is a register which is trashed i.e. modified in unpredictable way by inline assembler. This usually happens when you need a temp. register or use particular instruction which happens to modify some register as a by-product.
 */
 //gcc -o helloasm hello.c
-int main(){
+// int main(){
 
-   int x = 1;
+// //    int x = 1;
+
+// //    printf("Before asm, x = %d\n",x);
+
+// //     asm ("movl %1, %%eax;"
+// //          "movl %%eax, %0;"
+// //          :"=r"(x) /* x is output operand and it's related to %0 */
+// //          :"r"(11)  /* 11 is input operand and it's related to %1 */
+// //          :"%eax"); /* %eax is clobbered register */
+
+// //    printf("After asm,  x = %d\n", x);
+//     AsmMovl();
+// }
+
+
+
+void AsmMovl(){
+     int x = 1;
 
    printf("Before asm, x = %d\n",x);
 
--- AdvancedGoProgramming/ChapterTwo/AsmTest/main.go
+++ AdvancedGoProgramming/ChapterTwo/AsmTest/main.go
@@ -2,9 +2,9 @@ package main
 
 //This will need to be built with the whole package ( go build)
 
-//void SayHello(const char* s);
+//void AsmMovl();
 import "C"
 
 func main() {
-	C.SayHello(C.CString("Hello, World\n"))
+	C.AsmMovl()
 }
