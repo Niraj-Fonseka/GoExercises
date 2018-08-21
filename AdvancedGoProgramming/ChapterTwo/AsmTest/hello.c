1aaff69d38d7be29292e64840128110aeb582b21
MNF4691 <m_n_fonseka@homedepot.com>
2018-08-21 11:44:42 -0500

    Writing some asm

---
+++ AdvancedGoProgramming/ChapterTwo/AsmTest/hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/*
+A clobbered register is a register which is trashed i.e. modified in unpredictable way by inline assembler. This usually happens when you need a temp. register or use particular instruction which happens to modify some register as a by-product.
+*/
+//gcc -o helloasm hello.c
+int main(){
+
+   int x = 1;
+
+   printf("Before asm, x = %d\n",x);
+
+    asm ("movl %1, %%eax;"
+         "movl %%eax, %0;"
+         :"=r"(x) /* x is output operand and it's related to %0 */
+         :"r"(11)  /* 11 is input operand and it's related to %1 */
+         :"%eax"); /* %eax is clobbered register */
+
+   printf("After asm,  x = %d\n", x);
+}
