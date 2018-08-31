28b7bd3cbda85e98f9d481cbceff62ec4a689dcf
MNF4691 <m_n_fonseka@homedepot.com>
2018-08-31 07:08:20 -0500

    Adding go opencv

--- WebAssmCalculator/main.go
+++ WebAssmCalculator/main.go
@@ -1,33 +1,39 @@
 package main
 
 import (
-	"syscall/js"
 	"strconv"
+	"syscall/js"
 )
 
 func add(i []js.Value) {
 
-	value1 := js.Global().Get("document").Call("getElementById" , i[0].String()).Get("value").String()
-	value2 := js.Global().Get("document").Call("getElementById" , i[1].String()).Get("value").String()
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
 
-	int1 , _ := strconv.Atoi(value1)
-	int2 , _ := strconv.Atoi(value2)
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
 
-	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value",int1+int2)
+	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1+int2)
 }
 
-func substract(i []js.Value){
-	js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
-	println(js.ValueOf(i[0].Int() - i[1].Int()).String())
+func substract(i []js.Value) {
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1-int2)
 }
+
 func registerCallbacks() {
-	js.Global().Set("add",js.NewCallback(add))
-	js.Global().Set("substract",js.NewCallback(substract))
+	js.Global().Set("add", js.NewCallback(add))
+	js.Global().Set("substract", js.NewCallback(substract))
 
 }
 
-func main(){
-	c := make(chan struct{},0)
+func main() {
+	c := make(chan struct{}, 0)
 	println("Go WebAssembly Initialized")
 	registerCallbacks()
 
