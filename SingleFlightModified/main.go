15797edfab933ec561903474c534f02feca1c71d
MNF4691 <m_n_fonseka@homedepot.com>
2018-09-26 16:47:04 -0500

    Adding the modified singleflight code

---
+++ SingleFlightModified/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"GoExercises/SingleFlightModified/singleflight"
+	"fmt"
+	"log"
+	"time"
+)
+
+type SingleFlightGroup struct {
+	RequestGroup *singleflight.Group
+}
+
+func main() {
+	fmt.Println("Starting SingleFlightModified application")
+	//RequestFunctionsTesting()
+
+	fmt.Println("Press Ctrl + C to terminate the program")
+	fmt.Scanln()
+
+}
+
+func AddToRequestGroup(key string) {
+	value, err, _ := typ.RequestGroup.Do(key, func() (interface{}, error) {
+		go func() {
+			time.Sleep(60 * time.Second)
+			log.Println("Deleting key ", key)
+			typ.RequestGroup.Forget(key)
+		}()
+
+		return finalData, nil
+	})
+
+}
+
+func RequestFunctionsTesting() {
+	RequestOne(2 * time.Second)
+	RequestTwo(5 * time.Second)
+	RequestThree(10 * time.Second)
+
+}
+
+func (typ RequestOne(seconds time.Duration) string {
+	fmt.Println("Starting RequestOne function")
+	defer DoneExecutingFunction("RequestOne Exit")
+	time.Sleep(seconds)
+	return "RequestOne -> Done"
+}
+
+func RequestTwo(seconds time.Duration) string {
+	fmt.Println("Starting RequestTwo function")
+	defer DoneExecutingFunction("RequestTwo Exit")
+
+	time.Sleep(seconds)
+	return "RequestTwo -> Done"
+}
+
+func RequestThree(seconds time.Duration) string {
+	fmt.Println("Starting RequestTwo function")
+	defer DoneExecutingFunction("RequestThree Exit")
+
+	time.Sleep(seconds)
+	return "RequestThree -> Done"
+}
+
+func DoneExecutingFunction(terminateMessage string) {
+	fmt.Printf("Done Executing : %s \n", terminateMessage)
+}
