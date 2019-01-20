785511f44f9b80c8afb8916aa6955d33781788ee
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-01-19 20:00:39 -0600

    Adding BFS implementation

--- InterviewQuestions/GraphAlgos/BFS_DFS.go
+++ InterviewQuestions/GraphAlgos/BFS_DFS.go
@@ -1,28 +1,64 @@
 package main
 
-func DepthFirstSearch(adjList map[int][]int) {
+import "fmt"
+import "time"
 
-	var Q []int
+func DepthFirstSearch(adjList map[int][]int, root int, find int) {
 
-	//insert Q : Q = append(Q , value)
-	//remove Q : Q = Q[1:len(Q)]
-
+	var Queue []int
 	var visited []int
+	fmt.Println("Root : ", root)
+	Queue = append(Queue, root)
 
-	for parent, children := range adjList {
-		visited = append(visited, parent)
+	fmt.Println("Stating Loop")
+	for len(Queue) != 0 {
+		fmt.Println("Before removing the element : ", Queue)
+		dequeuedElement := Queue[0]
+		fmt.Printf("Removed element : %d \n", dequeuedElement)
+		Queue = Queue[1:len(Queue)]
+		fmt.Println("New Queue after removed : ", Queue)
 
-		Q = append(Q, parent)
-		for _, child := range children {
-			//check in visitedd
+		visited := append(visited, dequeuedElement)
+		fmt.Printf("Visiting element : %d \n", dequeuedElement)
+		fmt.Println("List of visted : ", visited)
+
+		fmt.Println("Iterating over the children :  ", adjList[dequeuedElement])
+		for _, child := range adjList[dequeuedElement] {
+			fmt.Printf("Child : %d \n", child)
+			fmt.Println("Before checking for visited ")
+			fmt.Println("Visisted ", visited)
+			isVisited := false
+
+			//check if the node is previously visited
+			for _, visitedNode := range visited {
+				if child == visitedNode {
+					isVisited = true
+				}
+			}
+
+			if !isVisited {
+				visited = append(visited, child)
+				Queue = append(Queue, child)
+			}
 		}
+		fmt.Println()
+		time.Sleep(5 * time.Second)
 	}
+
+	// for parent, children := range adjList {
+	// 	visited = append(visited, parent)
+
+	// 	Q = append(Q, parent)
+	// 	for _, child := range children {
+	// 		//check in visitedd
+	// 	}
+	// }
 
 }
 
 func main() {
 
-	graph := [][]int{{1, 2}, {2, 3}, {5}, {0}, {5}, {}, {}}
+	graph := [][]int{{1, 2, 3}, {}, {4, 5}, {}, {}, {6, 7}}
 
 	adjList := make(map[int][]int)
 	for row_index, row := range graph {
@@ -35,6 +71,7 @@ func main() {
 
 	}
 
-	DepthFirstSearch(adjList)
+	fmt.Println(adjList)
+	DepthFirstSearch(adjList, 0)
 
 }
