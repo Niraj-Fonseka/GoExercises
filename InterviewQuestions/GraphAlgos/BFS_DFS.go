af55b4a6026f176f3e568a10d55b78d54302b520
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-01-21 07:01:25 -0600

    Still working on evaluate

--- InterviewQuestions/GraphAlgos/BFS_DFS.go
+++ InterviewQuestions/GraphAlgos/BFS_DFS.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func DepthFirstSearch(adjList map[int][]int, root int, find int) {
+func DepthFirstSearchDetecCycle(adjList map[int][]int, root int, find int) {
 
 	var Queue []int
 	visited := make(map[int]bool)
@@ -22,6 +22,11 @@ func DepthFirstSearch(adjList map[int][]int, root int, find int) {
 
 		for _, child := range adjList[dequeuedElement] {
 			isVisited := false
+
+			if visited[child] {
+				//detect cycle
+
+			}
 
 			if child == find {
 				fmt.Println("Found : %d \n", find)
@@ -46,9 +51,97 @@ func DepthFirstSearch(adjList map[int][]int, root int, find int) {
 	fmt.Println(visited)
 }
 
+func DepthFirstSearch(adjList map[int][]int, root int, find int) {
+
+	var Queue []int
+	visited := make(map[int]bool)
+
+	Queue = append(Queue, root)
+	visited[root] = true
+
+	fmt.Println("Stating Loop")
+	for len(Queue) != 0 {
+		dequeuedElement := Queue[0]
+		Queue = Queue[1:len(Queue)]
+
+		visited[dequeuedElement] = true
+
+		fmt.Printf("Checking %d 's children \n", dequeuedElement)
+		fmt.Println("Queue before : %v \n", Queue)
+		for _, child := range adjList[dequeuedElement] {
+			isVisited := false
+
+			// if child == find {
+			// 	fmt.Println("Found : %d \n", find)
+			// 	break
+			// }
+
+			//check if the node is previously visited
+			for visitedNode, _ := range visited {
+				if child == visitedNode {
+					isVisited = true
+				}
+			}
+
+			if !isVisited {
+				visited[child] = true
+				fmt.Println("visiting : ", child)
+				Queue = append(Queue, child)
+
+			}
+		}
+	}
+	fmt.Println("Visited : ")
+	fmt.Println(visited)
+}
+
+func DepthFirstSearchStack(adjList map[int][]int, root int, find int) {
+
+	var Stack []int
+	visited := make(map[int]bool)
+
+	Stack = append(Stack, root)
+	visited[root] = true
+
+	fmt.Println("Stating Loop")
+	for len(Stack) != 0 {
+		dequeuedElement := Stack[len(Stack)-1]
+		Stack = Stack[0 : len(Stack)-1]
+
+		visited[dequeuedElement] = true
+
+		fmt.Printf("Checking %d 's children \n", dequeuedElement)
+		fmt.Println("Queue before : %v \n", Stack)
+		for _, child := range adjList[dequeuedElement] {
+			isVisited := false
+
+			// if child == find {
+			// 	fmt.Println("Found : %d \n", find)
+			// 	break
+			// }
+
+			//check if the node is previously visited
+			for visitedNode, _ := range visited {
+				if child == visitedNode {
+					isVisited = true
+				}
+			}
+
+			if !isVisited {
+				visited[child] = true
+				fmt.Println("visiting : ", child)
+				Stack = append(Stack, child)
+
+			}
+		}
+	}
+	fmt.Println("Visited : ")
+	fmt.Println(visited)
+}
+
 func main() {
 
-	graph := [][]int{{1, 2, 3}, {}, {4, 5}, {}, {}, {6, 7}}
+	graph := [][]int{{1, 2, 3}, {}, {4, 5}, {}, {0}, {6, 7}}
 
 	adjList := make(map[int][]int)
 	for row_index, row := range graph {
@@ -62,6 +155,6 @@ func main() {
 	}
 
 	fmt.Println(adjList)
-	DepthFirstSearch(adjList, 0, 5)
+	DepthFirstSearchStack(adjList, 0, 5)
 
 }
